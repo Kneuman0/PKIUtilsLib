efcc7bc4efe2d32324ed428a9f78c35e31b60e08
Kneuman0 <kylepneuman@gmail.com>
2017-07-18 00:18:34 -0400

    initial push

---
+++ src/main/java/fun/personalacademics/popup/PasswordPopup.java
@@ -0,0 +1,28 @@
+package fun.personalacademics.popup;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+
+public class PasswordPopup extends Alert{
+	
+	private TextField passwordTextBox;
+
+	public PasswordPopup() {
+		super(AlertType.CONFIRMATION);
+		setTitle("Enter Password");
+		setHeaderText("Please enter password");
+		passwordTextBox = new TextField();
+		GridPane pane = new GridPane();
+		pane.addRow(0, passwordTextBox);
+		getDialogPane().setContent(pane);
+	}
+	
+	public String getPassword(){
+		return passwordTextBox.getText();
+	}
+
+}
